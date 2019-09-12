3bd72789a9eaa6d29ea1c4bd085d0d946eaff870
ali e el aissaoui <njcisco>
2019-09-12 00:31:21 -0400

    added 2 files

---
+++ students.java
@@ -0,0 +1,17 @@
+package students;
+public class students {
+	public int Age;
+	public String Name;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		students ali = new students();
+		ali.Age = 20;
+		ali.Name = "alan";
+		System.out.println(ali.Age);
+		System.out.println(ali.Name);
+
+
+	}
+
+}
---
+++ te.java
@@ -0,0 +1,26 @@
+package gh;
+
+public class te {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int k = 0; k<20; k++) {	
+			int count = 0;
+			float floatcount = 0.0f;
+			//float total = 0f;
+		
+			for (int i = 0; i<10 ; i++);{
+				count++;
+				floatcount += 0.1f;
+		}
+		System.out.println("Count: " + count);
+		System.out.println("Float count: " + floatcount);
+		if (floatcount == 1) {
+			System.out.println("it's actually 10!");
+		}
+
+		
+	}
+
+}
+}
